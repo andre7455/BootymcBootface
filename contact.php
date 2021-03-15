98312bbe97def0f56e7092dd30aad8642a06b193
deathknight712 <62054978+deathknight712@users.noreply.github.com>
2021-03-15 11:55:42 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -11,12 +11,18 @@ include "includes\header.php";
         <p>sam@boatymcboatface.com</p>
 </div>
 
+<br>
+</br>
+
 <div>
     <h1>Telefoonnummer</h1>
         <p>06-12345678</p>
         <p>06-23456789</p>
         <p>06-34567891</p>
 </div>
+
+<br>
+</br>
 
 <div>
     <h1>Adres</h1>
@@ -25,13 +31,15 @@ include "includes\header.php";
         <p>Amsterdam</p>
 </div>
 
+<br>
+</br>
+
 <div>
     <h1>Namen</h1>
         <p>Marco Bloemers</p>
         <p>Andre van der Lugt</p>
         <p>Sam Niehaus</p>
 </div>
-
 
 <?php
 
