3fc951cfb4ab6c3f2581f57d9c787cf74ab345a5
SamNiehausStage <sam.niehaus.stage@outlook.com>
2021-03-15 12:02:54 +0100

    footer op de bodem

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,4 @@
-HTML
-
-<footer class="bg-light text-center text-lg-start">
+<footer class="bg-light text-center text-lg-start" id="footer">
 <!-- Copyright -->
 <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
 © 2020 Copyright:
