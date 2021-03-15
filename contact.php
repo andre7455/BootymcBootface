d3366c4eaf6fdf6b72f5b8de2f7b8abd034a2b5d
deathknight712 <62054978+deathknight712@users.noreply.github.com>
2021-03-15 10:44:15 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,40 @@
+<?php
+
+include "includes\header.php";
+
+?>
+
+<div>
+    <h1>Eamil</h1>
+        <p>marco@boatymcboatface.com</p>
+        <p>andre@boatymcboatface.com</p>
+        <p>sam@boatymcboatface.com</p>
+</div>
+
+<div>
+    <h1>Telefoonnummer</h1>
+        <p>06-12345678</p>
+        <p>06-23456789</p>
+        <p>06-34567891</p>
+</div>
+
+<div>
+    <h1>Adres</h1>
+        <p>kaastraat 12</p>
+        <p>1234 AH</p>
+        <p>Amsterdam</p>
+</div>
+
+<div>
+    <h1>Namen</h1>
+        <p>Marco Bloemers</p>
+        <p>Andre van der Lugt</p>
+        <p>Sam Niehaus</p>
+</div>
+
+
+<?php
+
+include "includes/footer.php";
+
+?>
