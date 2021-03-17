58a61ecba3b30d69f937f40b9d301e43c48abd06
deathknight712 <62054978+deathknight712@users.noreply.github.com>
2021-03-17 10:17:21 +0100

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,4 +3,17 @@ include "includes\header.php";
 include "includes\DatabaseConfig.php";
 ?>
 
-<p> wij zijn kaasmannen van de hoogste rang </p>
+<div>
+    <h1>Wie zijn wij</h1>
+        <p>CoralYachts verhuurd zeiljachten vanuit diverse havens in binnen- en buitenland. Er kand door klanten<br>
+            een eenvoudige zeilboot worden gehuurd voor een dagje uit, maar er kan ook een groter jacht worden<br>
+            gehuurd, inclusief schipper en catering, voor meerede dagen, voor een volledige vakantie.
+        </p>
+        
+</div>
+
+<?php
+
+include "includes/footer.php";
+
+?>
