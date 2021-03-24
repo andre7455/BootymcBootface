3cdd9642c2fa1eb2f9dc98d7c535db1a7763d49a
SamNiehausStage <sam.niehaus.stage@outlook.com>
2021-03-24 15:41:56 +0100

    login registratie werkt

--- Version/LoginController.php
+++ Version/LoginController.php
@@ -1,37 +1,36 @@
 <?php
 include "DatabaseConfig.php";
 
-class LoginController{
+$dbconfig = new DatabaseConfig;
+$email = $_POST["loginEmail"];
+$password = $_POST["loginPassword"];
 
-    public static function login(){
+if(isset($_POST["loginSubmit"])){
+    $userQuery = "SELECT * FROM gebruiker WHERE Email= ?";
+    $userResult = $dbconfig->connect()->prepare($userQuery);
+    $userResult->execute(array($email));
+    $userRow = $userResult->setFetchMode(PDO::FETCH_ASSOC);
+    $userRow = $userResult->fetchAll();
 
-        $dbconfig = new DatabaseConfig;
-        $email = $_POST["loginEmail"];
-        $password = $_POST["loginPassword"];
-
-        if(isset($_POST["loginSubmit"])){
-            $userQuery = "SELECT * FROM gebruiker WHERE Email= ?";
-            $userResult = $dbconfig->connect()->prepare($userQuery);
-            $userResult->execute(array($email));
-            $userRow = $userResult->setFetchMode(PDO::FETCH_ASSOC);
-            $userRow = $userResult->fetchAll();
-
-            if(!empty($userRow)){
-                $i = 0;
-                $rowLength = count($userRow);
-                while ($i <= $rowLength) {
-                    if (password_verify($password, $userRow[$i]['Wachtwoord'])) {
-                        $_SESSION["userEmail"] = $userRow[$i]['Email'];
-                        $_SESSION["username"] = $userRow[$i]['Username'];
-                        $_SESSION["userRole"] = $userRow[$i]['Rol'];
-                    }
-                    $i++;
-
-                    if ($i == $rowLength) {
-                        //foutmelding
-                    }
-                }                
+    if(!empty($userRow)){
+        $i = 0;
+        $rowLength = count($userRow);
+        while ($i <= $rowLength) {
+            if (password_verify($password, $userRow[$i]['Wachtwoord'])) {
+                @session_start();
+                $_SESSION["userEmail"] = $userRow[$i]['Email'];
+                $_SESSION["username"] = $userRow[$i]['Username'];
+                $_SESSION["userRole"] = $userRow[$i]['Rol'];
+                header("Location: index.html");
             }
-        }
+            $i++;
+            if ($i == $rowLength) {
+                //foutmelding
+                print_r("Er is iets fout gegaan, neem contact op met de systeembeheerder");
+            }
+            print_r("Er is iets fout gegaan, neem contact op met de systeembeheerder");
+        }    
+        print_r("Er is iets fout gegaan, neem contact op met de systeembeheerder");   
     }
+    print_r("Er is iets fout gegaan, neem contact op met de systeembeheerder");
 }
--- Version/RegistreerController.php
+++ Version/RegistreerController.php
@@ -1,18 +1,21 @@
 <?php
 include "DatabaseConfig.php";
 
-class RegistreerController{
 
-    public static function login(){
+$dbconfig = new DatabaseConfig;
 
-        $dbconfig = new DatabaseConfig;
-        
-        $email = $_POST["registreerEmail"];
-        $username = $_POST["registreerUsername"];
-        $password = $_POST["registreerPassword"];
-        $passwordConfirmed = $_POST["registreerPasswordConfirmed"];
+$email = $_POST["registreerEmail"];
+$username = $_POST["registreerUsername"];
+$password = $_POST["registreerPassword"];
+$passwordConfirmed = $_POST["registreerPasswordConfirmed"];
 
-        if ($password === $passwordConfirmed) {
-        }
-    }
+if ($password === $passwordConfirmed) {
+    $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
+    
+    $sql = "INSERT INTO gebruiker( `Username`, `Email`, `Wachtwoord`) VALUES ('" . $username . "', '" . $email . "', '" . $passwordHashed . "');";
+                
+    $stmt = $dbconfig->connect()->prepare($sql);
+    $stmt->execute();
 }
+
+header("Location: index.html");
