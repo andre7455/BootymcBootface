9017985bdb72639f9e1bc9c162b9150aa29f95b0
andre7455 <42913113+andre7455@users.noreply.github.com>
2021-04-07 14:08:26 +0200

    Update addboat2.php

--- Version/addboat2.php
+++ Version/addboat2.php
@@ -28,22 +28,28 @@
     include 'header.php';
     include 'DatabaseConfig.php'
 ?>
-
+<br><br><br><br><br><br>
 
 <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label>Boat name</label>
+    <input type="text" class="form-control" id="name" placeholder="Costa Concordia">
   </div>
+
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label>Beschijving</label>
+    <input type="textbox" class="form-control input-large" id="beschijving" placeholder="Password">
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+  <div class="form-group">
+    <label for=""></label>
   </div>
+
+  <div class="form-group">
+    <label>prijs</label>
+    <input type="text" class="form-control" id="prijs" placeholder="99.99">
+  </div>
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
